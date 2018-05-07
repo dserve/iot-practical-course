c8145eb3e88fe70b2fa747a5b1f26050f85c130a
Dominik Serve <serve@itq.de>
2018-05-07 06:43:13 +0200

    rm sudo su

--- startup.sh
+++ startup.sh
@@ -24,48 +24,47 @@ df -Th
 cd ..
 
 # remove lock files to prevent apt-get update from failing
-sudo su
-rm /var/cache/apt/archives/lock
-rm /var/lib/dpkg/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
 # install docker-ce
-apt-get update
-apt-get install \
+sudo apt-get update
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common -y
 
 # increase virtual memory size for elasticsearch
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
 # accept incoming messages for kafka
 #iptables -I INPUT -p tcp -m tcp --dport 9092 -j ACCEPT
 
 # Install Java
-add-apt-repository -y ppa:webupd8team/java
-apt update
-apt install oracle-java8-installer -y
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-installer -y
 
 # Zookeeper
-apt install zookeeperd -y
+sudo apt install zookeeperd -y
 
 # Kafka and relevant packages
 
 wget http://apache.lauf-forum.at/kafka/1.1.0/kafka_2.12-1.1.0.tgz
-tar -xvf kafka_2.12-1.1.0.tgz -C /opt
+sudo tar -xvf kafka_2.12-1.1.0.tgz -C /opt
 rm kafka_2.12-1.1.0.tgz
-apt install python3-pip
-pip3 install kafka-python
+sudo apt install python3-pip
+sudo pip3 install kafka-python
 cd /opt/kafka_2.12-1.1.0
-./bin/kafka-server-start.sh &
+sudo ./bin/kafka-server-start.sh &
 cd ~
 
 # Flink
 
 wget http://apache.lauf-forum.at/flink/flink-1.4.2/flink-1.4.2-bin-hadoop28-scala_2.11.tgz
-tar -xvf flink-1.4.2-bin-hadoop28-scala_2.11.tgz -C /opt
+sudo tar -xvf flink-1.4.2-bin-hadoop28-scala_2.11.tgz -C /opt
 rm flink-1.4.2-bin-hadoop28-scala_2.11.tgz
 cd /opt/flink-1.4.2-bin-hadoop28-scala_2.11
-./bin/start-local &
+sudo ./bin/start-local &
 cd ~
 
 # Elasticsearch:
@@ -82,9 +81,9 @@ cd ~
 
 # Maven
 
-apt-get install maven
+sudo apt-get install maven
 
 # Node & NPM
 
-apt install nodejs
-apt install npm
+sudo apt install nodejs
+sudo apt install npm
