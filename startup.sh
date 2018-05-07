2f2c217b784e798325c97803e332722304f932d5
Dominik Serve <serve@itq.de>
2018-05-07 11:17:35 +0200

    add sleep

--- startup.sh
+++ startup.sh
@@ -44,6 +44,16 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt update
 sudo apt install oracle-java8-installer -y
 
+# Maven
+
+sudo apt-get install maven -y
+
+# Node & NPM
+
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo ln -s `which nodejs` /usr/bin/node
+
 # Zookeeper
 sudo apt install zookeeperd -y
 
@@ -62,7 +72,8 @@ rm kafka_2.12-1.1.0.tgz
 sudo apt install python3-pip -y
 sudo pip3 install kafka-python
 cd /opt/kafka_2.12-1.1.0
-sudo ./bin/kafka-server-start.sh
+sudo ./bin/kafka-server-start.sh ./config/server.properties &
+sleep 20s
 ./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic testing
 ./bin/kafka-topics.sh --list --zookeeper localhost:2181
 ./bin/kafka-consoleproducer.sh --broker-list localhost:9092 --topic testing
@@ -76,6 +87,7 @@ sudo tar -xvf flink-1.4.2-bin-hadoop28-scala_2.11.tgz -C /opt
 rm flink-1.4.2-bin-hadoop28-scala_2.11.tgz
 cd /opt/flink-1.4.2-bin-hadoop28-scala_2.11
 sudo ./bin/start-local &
+sleep 20s
 wget -O hamlet.txt http://www.gutenberg.org/cache/epub/1787/pg1787.txt
 ./bin/flink run ./examples/batch/WordCount.jar --file://opt/flink-1.4.2-bin-hadoop28-scala_2.11/hamlet.txt
 cd ~
@@ -96,19 +108,11 @@ cd ~
 tar -xvf elasticsearch-5.6.3.tar.gz -C /opt
 rm elasticsearch-5.6.3.tar.gz
 cd /opt/elasticsearch-5.6.3
-su yourname -c "./bin/elasticsearch"
+su yourname -c "./bin/elasticsearch &"
+sleep 20s
 curl -XPUT 'localhost:9200/testindex?pretty&pretty'
 curl -XGET 'localhost:9200/_cat/indices?v&pretty'
 curl -XPUT 'localhost:9200/testindex/agents/1?pretty&pretty' -H 'Content-Type: application/json' -d ' { "name": "James Bond" } '
 curl -XGET 'localhost:9200/testindex/agents/1?pretty&pretty'
 cd ~
 
-# Maven
-
-sudo apt-get install maven -y
-
-# Node & NPM
-
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo ln -s `which nodejs` /usr/bin/node
